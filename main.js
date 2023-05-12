795263110c04636a791596536811691d294968f2
Jenny Lee <88498857+jenl11@users.noreply.github.com>
2023-05-12 02:23:06 -0700

    JS: arrow up

--- main.js
+++ main.js
@@ -37,6 +37,21 @@ document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
+// show arrow up button when scrolling down
+const arrowUp =document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight/2) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+});
+
+// handle click on arrow up
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#home');
+})
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: "smooth"});
